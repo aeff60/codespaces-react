4ad0c08ed9162363a6c15baef4e15dcf645d4418
Aef_SB <46419278+aeff60@users.noreply.github.com>
2023-11-17 18:34:44 +0000

    Add table to display coding resources

--- src/App.js
+++ src/App.js
@@ -1,29 +1,45 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.sampleapis.com/codingresources/codingResources')
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.log(error));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
+        <table>
+          <thead>
+            <tr>
+                <th>Description</th>
+                <th>Types</th>
+                <th>Topics</th>
+                <th>URL</th>
+              
+            </tr>
+          </thead>
+          <tbody>
+            {data.map(resource => (
+              <tr key={resource.id}>
+                <td>{resource.description}</td>
+                <td>{resource.types.join(', ')}</td>
+                <td>{resource.topics.join(', ')}</td>
+                <td><a href={resource.url}>อ่านเพิ่มเติม</a></td> 
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </header>
     </div>
   );
 }
+
 
 export default App;
